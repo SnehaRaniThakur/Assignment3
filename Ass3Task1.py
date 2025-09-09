9182cd7aebd17740701e628fbf10f000d9255eea
SnehaRaniThakur <sneharanithakur.jsr@gmail.com>
2025-09-09 16:58:45 +0530

    Add files via upload

---
+++ Ass3Task1.py
@@ -0,0 +1,10 @@
+
+def factorial(n):
+    if n==1 or n==0:
+        return 1
+    else:
+        fact= n* factorial(n-1)
+        return fact
+n=int(input('Enter a number: '))
+ans= factorial(n)
+print('Factorial of ',n,' is = ',ans)
